2f42abdded8195f1d93a3fd570551b2811c8adf2
Nihad-lab <mamedovnihad306@gmail.com>
2025-12-24 22:20:44 +0300

    65

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-function sumPositiveNums(a, b) {
-  if (typeof a !== "number" || typeof b !== "number") {
-    return "One of the arguments is not a number";
-  }
-  if (a <= 0 || b <= 0) {
-    return "Numbers are not positive";
-  }
-  return a + b;
-}
-console.log(sumPositiveNums(3, 8)); // 11
+let val = 11;
+const result = val >= 0 ? val : -val;
+console.log(result); // 11
 
-let month = 2;
-switch (month) {
-  case 12:
-  case 1:
-  case 2:
-    console.log("Winter");
-    break;
-  default:
-    console.log("Not a winter month");
+// Тернарный оператор в вызове функции
+console.log(val >= 0 ? val : -val); // 11
+for (let i = 0; i < 5; i++) {
+  console.log(i); // 0, 1, 2, 3, 4
 }
+const arr = ["frost", "second", "third"];
+arr.forEach((element, index) => console.log(`${index}: ${element}`));
+
+let i = 0;
+while (i < 5) {
+  console.log(i); // 0, 1, 2, 3, 4
+  i++;
+}
+let i = 0;
+do {
+  console.log(i); // 0
+  i++;
+} while (i < 0);
