01051ccba6ab214c8ab1870099a5e5a044708ddf
Nihad-lab <mamedovnihad306@gmail.com>
2025-12-24 22:01:52 +0300

    39

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-console.log(10 + " apples"); // "10 apples"
-// Function Declaration
-function sum(a, b) {
-  return a + b;
-}
-
-// Function Expression
-const multiply = function (a, b) {
-  return a * b;
-};
+const createPost = (post, createdAt = new Date()) => ({
+  ...post,
+  createdAt,
+});
+const post = { id: 1, title: "News" };
+console.log(createPost(post));
